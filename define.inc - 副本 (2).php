e210eb59210a8e6943e597b95fea65c742cb54c1
unknown <20111224i705@20111224i705-PC.(none)>
2013-05-13 16:42:50 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- define.inc - 副本 (2).php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('ET_URL','http://'.$_SERVER['HTTP_HOST'].'/');
-	//本地
-    define('SITE_ROOT_NAME','myerpcenter');
-	//客户端
-    define('CLIENT_ROOT_NAME','myerp');
-    define('CLIENT_URL','http://www.'.CLIENT_ROOT_NAME.'.com/');
-	
-?>
